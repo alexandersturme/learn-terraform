e0f23a2188513902d3f4fb32c2522dbb5a3d0ef7
Alexander Sturme <sasha_st@inbox.lv>
2024-10-26 12:36:04 +0300

    Adjusted configs

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "sasha-st-org"
 
-    workspaces {
-      name = "learn-terraform"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform"
+  #   }
+  # }
 
   required_providers {
     aws = {
